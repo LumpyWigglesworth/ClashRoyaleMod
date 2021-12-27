6bc5fc6f527bdf20bac30f44cab97bfc270c3351
LumpyWigglesworth <david@endikos.com>
2021-12-26 23:32:02 -0600

    Added The Log

---
+++ Items/LogCard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using ClashRoyaleMod.Projectiles;
+
+namespace ClashRoyaleMod.Items
+{
+    class LogCard : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("The Log");
+            Tooltip.SetDefault("A normal log gone berserk due to a rage spill.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 50;
+            item.width = 16;
+            item.height = 20;
+            item.magic = true;
+            item.noMelee = true;
+            item.mana = 20;
+            item.useStyle = ItemUseStyleID.HoldingUp;
+            item.useTime = 60;
+            item.useAnimation = 60;
+            item.value = Item.buyPrice(gold: 10);
+            item.rare = ItemRarityID.Cyan;
+            item.shoot = ModContent.ProjectileType<TheLog>();
+            item.shootSpeed = 10;
+            item.UseSound = SoundID.Item19;
+        }
+    }
+}
--- Projectiles/TheLog.cs
+++ Projectiles/TheLog.cs
@@ -12,7 +12,9 @@ namespace ClashRoyaleMod.Projectiles
 {
     class TheLog : ModProjectile
     {
-        float duration = 180f;
+        protected float spinRate = (float)Math.PI/30;
+        protected float bounce = -0.65f;
+        //float lateralSpeed = 10f;
         public override void SetDefaults()
         {
             projectile.width = 60;
@@ -21,26 +23,49 @@ namespace ClashRoyaleMod.Projectiles
             projectile.magic = true;
             projectile.hostile = false;
             projectile.friendly = true;
+            projectile.ignoreWater = true;
             projectile.tileCollide = false;
-            projectile.ignoreWater = false;
+            projectile.localAI[1] = 0f;
+            projectile.localAI[0] = 0f;
+            projectile.timeLeft = 180;
         }
         public override void AI()
         {
             Player p = Main.player[projectile.owner];
-
-            if (projectile.localAI[0] >= duration)
-                projectile.Kill();
+            projectile.tileCollide = false;
 
             if (projectile.localAI[0] == 0f)
-                projectile.localAI[1] = p.Center.X + p.height / 2;
+            {
+                projectile.position = p.Center + new Vector2(0, -100f);
+                projectile.localAI[1] = p.Center.Y;
+                projectile.localAI[0]++;
+            }
 
-            Vector2 lateralVelocity = new Vector2(10, 0);
-            Vector2 gravity = new Vector2(0, p.gravity);
+            Vector2 newVelocity = new Vector2(projectile.velocity.X, projectile.velocity.Y + p.gravity);
+            if (projectile.position.Y > projectile.localAI[1] - projectile.height / 2)
+            {
+                newVelocity.Y *= bounce;
+                projectile.position.Y = projectile.localAI[1] - projectile.height / 2;
+                Main.PlaySound(SoundID.Dig, projectile.position);
+            }
+            projectile.velocity = newVelocity;
+            projectile.rotation += spinRate * projectile.direction;
         }
-        public override bool? CanCutTiles()
+
+        public override void Kill(int timeLeft)
         {
-            return true;
+            for (int i = 0; i < 50; i++)
+            {
+                Dust.NewDust(projectile.position, projectile.width, projectile.height, 7);
+            }
+            Main.PlaySound(SoundID.Item14, projectile.position);
         }
-
+        /*
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            projectile.velocity = new Vector2(oldVelocity.X, oldVelocity.Y * bounce);
+            return false;
+        }
+        */
     }
 }
